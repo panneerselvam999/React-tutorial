d3b53187ddd57ed737417dc410e2b0459887dc26
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-06 15:59:23 -0500

    coditional rendering 2

--- app/src/Component/Garage.js
+++ app/src/Component/Garage.js
@@ -3,17 +3,19 @@ import Apple from "./Apple";
 
 
 // const bmw = "BMW"
-// const CarInfo = { brand: "BMW", color: "Red" }
-const CarInfo = {}
+const CarInfo = { brand: "BMW", color: "Red" }
+// const CarInfo = {}
+const CarInfoVal = CarInfo.brand !== undefined && CarInfo.color !== undefined
 const AppleInfo = { type: "Mobile", color: "Silver" }
-
+const door = false;
 function Garage() {
     return (
         <div>
             <h1>This is Garage Component</h1>
-            {CarInfo.brand !== undefined && CarInfo.color !== undefined ?
-                <Car CarInfoBMW={CarInfo} /> : null}
+            {CarInfoVal ? <Car CarInfoBMW={CarInfo} /> : null}
             <Apple AInfo={AppleInfo} />
+            {door ? <p>Door is Open</p> : <p>Door is Close</p>}
+
         </div>
     )
 }
