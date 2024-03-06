640f115c8e730f10d360838b501e9b7d52fabb95
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-06 16:36:56 -0500

    print list

--- app/src/Component/Garage.js
+++ app/src/Component/Garage.js
@@ -12,6 +12,8 @@ const carList = [
     { brand: "BMW", color: "Green" }
 ]
 
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 const CarInfoVal = CarInfo.brand !== undefined && CarInfo.color !== undefined
 const AppleInfo = { type: "Mobile", color: "Silver" }
 const door = false;
@@ -23,8 +25,17 @@ function Garage() {
             <Apple AInfo={AppleInfo} />
             {door ? <p>Door is Open</p> : <p>Door is Close</p>}
             <ul>
-                {carList.map((obj) => < li > <Car CarInfo={obj} /> </li>)}
+                {carList.map((obj) => < li key={obj.brand}> <Car CarInfo={obj} /> </li>)}
             </ul>
+
+            <ul>
+                {numberList.map((ele, index) => <p key={index}>{ele}</p>)}
+            </ul>
+            {/* <ul>
+                {numberList.map((ele) => (
+                    <p key={ele}>{ele}</p>
+                ))}
+            </ul> */}
 
         </div >
     )
