78b177a25951c5932733cd6b0e88f34e37ce4ca7
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 14:50:41 -0500

    Simplifying onChange handles

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -1,73 +1,33 @@
 import { useState } from "react";
 
 function Form() {
-    // const [name, setName] = useState("");
-    // const [age, setAge] = useState("");
-    // const [email, setEmail] = useState("");
-
     const [input, setInput] = useState({})
-
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log("form submit successfully")
-        // console.log("current State : ", name, age, email)
         console.log(input);
     }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
+    function handleChange(e) {
+        const name = e.target.name
+        const value = e.target.value
+        setInput(
+            (pre) => {
+                return { ...pre, { name }: value
+            }
+            }
+        )
+}
+return (
+    <>
+        <form onSubmit={handleSubmit}>
 
-                <label>Name: <input type="text" onChange={
-                    (e) => {
-                        setInput(
-                            (pre) => {
-                                return { ...pre, name: e.target.value }
-                            }
-                        )
-                    }
-                } /></label> <br />
-
-                <label>Age: <input type="text" onChange={
-                    (e) => {
-                        setInput(
-                            (pre) => {
-                                return { ...pre, age: e.target.value }
-                            }
-                        )
-                    }
-                } /></label> <br />
-
-                <label>Email: <input type="email" onChange={
-                    (e) => {
-                        setInput(
-                            (pre) => {
-                                return { ...pre, email: e.target.value }
-                            }
-                        )
-                    }
-                } /></label> <br />
-
-
-
-
-                {/* <label>Name: <input type="text" onChange={(e) => { setInput({ name: e.target.value }) }} /></label> <br />
-
-                <label>Age: <input type="text" onChange={(e) => { setInput({ age: e.target.value }) }} /></label> <br />
-
-                <label>Email: <input type="email" onChange={(e) => { setInput({ email: e.target.value }) }} /></label> <br /> */}
-
-
-                {/* <label>Name: <input type="text" onChange={(e) => { setName(e.target.value) }} /></label> <br />
-
-                <label>Age: <input type="text" onChange={(e) => { setAge(e.target.value) }} /></label> <br />
-
-                <label>Email: <input type="email" onChange={(e) => { setEmail(e.target.value) }} /></label> <br /> */}
-
-                <input type="submit" />
-            </form>
-        </>
-    )
+            <label>Name: <input type="text" name="name" onChange={handleChange} /></label> <br />
+            <label>Age: <input type="text" name="age" onChange={handleChange} /></label> <br />
+            <label>Email: <input type="email" name="email" onChange={handleChange} /></label> <br />
+            <input type="submit" />
+        </form>
+    </>
+)
 }
 export default Form;
