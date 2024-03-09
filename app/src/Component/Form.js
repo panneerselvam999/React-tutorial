61acd2a9f8f6e247f3d026eed82bce9a7bf35740
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 15:27:00 -0500

    text Area

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -33,6 +33,8 @@ function Form() {
                     <option value="USA">USA</option>
                     <option value="Bangadesh">Bandadesh</option>
                 </select></label><br />
+
+                <label>About : <textarea name="about" onChange={handleChange} value={input.about}></textarea></label> <br />
                 <input type="submit" />
             </form>
         </>
@@ -40,75 +42,3 @@ function Form() {
 }
 export default Form;
 
-
-
-// import { useState } from "react";
-
-// function Form() {
-//     const [input, setInput] = useState({
-//         phone: "+91",
-//         country: "select", // Set the initial value for "country"
-//     });
-
-//     function handleSubmit(e) {
-//         e.preventDefault();
-//         console.log(input);
-//     }
-
-//     function handleChange(e) {
-//         const name = e.target.name;
-//         const value = e.target.value;
-//         setInput((prev) => ({
-//             ...prev,
-//             [name]: value,
-//         }));
-//     }
-
-//     return (
-//         <>
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                     Name: <input type="text" name="name" onChange={handleChange} />
-//                 </label>{" "}
-//                 <br />
-//                 <label>
-//                     Age: <input type="text" name="age" onChange={handleChange} />
-//                 </label>{" "}
-//                 <br />
-//                 <label>
-//                     Email:{" "}
-//                     <input
-//                         type="email"
-//                         name="email"
-//                         onChange={handleChange}
-//                         value={input.email}
-//                     />
-//                 </label>{" "}
-//                 <br />
-//                 <label>
-//                     Phone:{" "}
-//                     <input
-//                         type="text"
-//                         name="phone"
-//                         onChange={handleChange}
-//                         value={input.phone}
-//                     />
-//                 </label>{" "}
-//                 <br />
-//                 <label>
-//                     Country :{" "}
-//                     <select name="country" onChange={handleChange} value={input.country}>
-//                         <option value="select">Select</option>
-//                         <option value="India">India</option>
-//                         <option value="USA">USA</option>
-//                         <option value="Bangadesh">Bangladesh</option>
-//                     </select>
-//                 </label>
-//                 <br />
-//                 <input type="submit" />
-//             </form>
-//         </>
-//     );
-// }
-
-// export default Form;
