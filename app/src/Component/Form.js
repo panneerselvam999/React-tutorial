2e86dd22ad4134f08829f483e67942ed5f5fb4d1
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 13:09:04 -0500

    form submit - 1

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -1,25 +1,37 @@
 import { useState } from "react";
 
 function Form() {
-    const [name, setName] = useState("");
-    function oChange(e) {
-        setName(
-            e.target.value
-        )
-    }
+    // const [name, setName] = useState("");
+    // const [age, setAge] = useState("");
+    // const [email, setEmail] = useState("");
+
+    const [input, setInput] = useState({})
+
 
     function handleSubmit(e) {
         e.preventDefault()
-            console.log("form submit successfully")
-        console.log("current State : ", name)
+        console.log("form submit successfully")
+        // console.log("current State : ", name, age, email)
+        console.log(input);
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                {/* <label htmlFor="">Enter your name: <input type="text" onChange={(e) => { setName(e.target.value) }} /></label> */}
-                <label htmlFor="">Enter your name: <input type="text" onChange={oChange} /></label>
-                <br />
+
+                <label>Name: <input type="text" onChange={(e) => { setInput(e.target.value) }} /></label> <br />
+
+                <label>Age: <input type="text" onChange={(e) => { setInput(e.target.value) }} /></label> <br />
+
+                <label>Email: <input type="email" onChange={(e) => { setInput(e.target.value) }} /></label> <br />
+
+
+                {/* <label>Name: <input type="text" onChange={(e) => { setName(e.target.value) }} /></label> <br />
+
+                <label>Age: <input type="text" onChange={(e) => { setAge(e.target.value) }} /></label> <br />
+
+                <label>Email: <input type="email" onChange={(e) => { setEmail(e.target.value) }} /></label> <br /> */}
+
                 <input type="submit" />
             </form>
         </>
