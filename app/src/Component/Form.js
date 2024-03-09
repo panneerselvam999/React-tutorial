2bbadfedb2b0b1cf02eb241cb4380f6c893ad27e
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 12:18:58 -0500

    Get Form input value

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function Form() {
+    const [name, setName] = useState("");
+    console.log("current State : ", name)
+    function oChange(e) {
+        setName(
+            e.target.value
+        )
+    }
     return (
         <>
             <form action="">
-                <label htmlFor="">Enter your name: <input type="text" /></label>
+                {/* <label htmlFor="">Enter your name: <input type="text" onChange={(e) => { setName(e.target.value) }} /></label> */}
+                <label htmlFor="">Enter your name: <input type="text" onChange={oChange} /></label>
             </form>
         </>
     )
