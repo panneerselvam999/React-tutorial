0a862db9ae6bc0436ff99cf457a3d1eae844ab1d
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 14:59:24 -0500

    Set default value in form

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Form() {
-    const [input, setInput] = useState({})
+    const [input, setInput] = useState({ phone: "+91", mail: "@gmail.com" })
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -13,21 +13,23 @@ function Form() {
         const value = e.target.value
         setInput(
             (pre) => {
-                return { ...pre, { name }: value
-            }
+                return {
+                    ...pre, [name]: value
+                }
             }
         )
-}
-return (
-    <>
-        <form onSubmit={handleSubmit}>
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
 
-            <label>Name: <input type="text" name="name" onChange={handleChange} /></label> <br />
-            <label>Age: <input type="text" name="age" onChange={handleChange} /></label> <br />
-            <label>Email: <input type="email" name="email" onChange={handleChange} /></label> <br />
-            <input type="submit" />
-        </form>
-    </>
-)
+                <label>Name: <input type="text" name="name" onChange={handleChange} /></label> <br />
+                <label>Age: <input type="text" name="age" onChange={handleChange} /></label> <br />
+                <label>Email: <input type="email" name="email" onChange={handleChange} value={input.mail} /></label> <br />
+                <label>Phone: <input type="text" name="phone" onChange={handleChange} value={input.phone} /></label> <br />
+                <input type="submit" />
+            </form>
+        </>
+    )
 }
 export default Form;
