0aaa4e62762ea25e70a41dad7cfc41dee5b6b7d7
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-09 12:33:19 -0500

    form submit

--- app/src/Component/Form.js
+++ app/src/Component/Form.js
@@ -2,17 +2,25 @@ import { useState } from "react";
 
 function Form() {
     const [name, setName] = useState("");
-    console.log("current State : ", name)
     function oChange(e) {
         setName(
             e.target.value
         )
     }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+            console.log("form submit successfully")
+        console.log("current State : ", name)
+    }
+
     return (
         <>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 {/* <label htmlFor="">Enter your name: <input type="text" onChange={(e) => { setName(e.target.value) }} /></label> */}
                 <label htmlFor="">Enter your name: <input type="text" onChange={oChange} /></label>
+                <br />
+                <input type="submit" />
             </form>
         </>
     )
