9c0b2877c4679b7a15f8802db8c6c16dbf653798
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-11 07:39:35 -0500

    Router Links

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./Route/Home.js"
 import About from "./Route/About.js"
 import Contact from "./Route/Contact.js"
@@ -10,6 +10,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ul>
+          <li><Link to="/">  Home</Link></li>
+          <li><Link to="/about">  About</Link></li>
+          <li><Link to="/contact">  Contact</Link></li>
+        </ul>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
