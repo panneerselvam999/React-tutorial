69b9586e12e56b58acf6c17563e986594124a207
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-03-11 07:28:26 -0500

    create route pages, set route

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,21 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Route/Home.js"
+import About from "./Route/About.js"
+import Contact from "./Route/Contact.js"
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
